4c7e4a1b0154bafc250d751ba91b56250e8393c4
Kyle Plovie <35747950+KylePlovie@users.noreply.github.com>
2025-05-07 15:05:51 -0400

    Add files via upload

---
+++ library-dashboard/src/components/ui/input.js
@@ -0,0 +1,8 @@
+export function Input(props) {
+  return (
+    <input
+      {...props}
+      className="w-full border border-gray-300 p-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
+    />
+  );
+}
