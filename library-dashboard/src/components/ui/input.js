22ae28368fd2cfce0887e6b2f4d7b4805b992634
Kyle Plovie <35747950+KylePlovie@users.noreply.github.com>
2025-05-07 20:27:33 -0400

    Delete library-dashboard directory

--- library-dashboard/src/components/ui/input.js
+++
@@ -1,8 +0,0 @@
-export function Input(props) {
-  return (
-    <input
-      {...props}
-      className="w-full border border-gray-300 p-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
-    />
-  );
-}
